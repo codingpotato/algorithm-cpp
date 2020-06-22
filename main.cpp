fa7c975cc4ddcf11108df8f31ef0c4777f806166
Yongwu <codingpotato@gmail.com>
2020-06-22 21:56:21 +0800

    Add doctest

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <doctest/doctest.h>
+
+#include <iostream>
+
+int app_main(int argc, char **argv) {
+  std::cout << "Application starting...\n";
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  doctest::Context context;
+  context.applyCommandLine(argc, argv);
+
+  int res = context.run();
+  if (context.shouldExit()) {
+    return res;
+  }
+
+  return res;
+}
